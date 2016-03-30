10e5efda6093f7c14530aecb7296cbd00cba3df4
simplon-gwenG <lambertleila@aol.com>
2016-03-30 11:01:48 +0200

    rotation images

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,12 +1,12 @@
 angular.module('app.controllers', ['ionic','ngCordova'])
 
 .controller('imageController', function($scope, $cordovaCamera, $cordovaFile) {
-    // 1
+    // 1 tableau pour le ng-repeat pour stocker les images
     $scope.images = [];
 
 
     $scope.addImage = function() {
-        // 2
+        // 2 options de tableau passé à cordova Camera
         var options = {
             destinationType : Camera.DestinationType.FILE_URI,
             sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
@@ -15,10 +15,10 @@ angular.module('app.controllers', ['ionic','ngCordova'])
             popoverOptions: CameraPopoverOptions,
         };
 
-        // 3
+        // 3 appelle le module ngCordova que l'on a injecté au controller
         $cordovaCamera.getPicture(options).then(function(imageData) {
 
-            // 4
+            // 4 qd l'image retourne des données on passe à la fonction SUCCESS qui appelle d'autres fonctions pour copier l'image originale dans le dossier de notre application
             onImageSuccess(imageData);
 
             function onImageSuccess(fileURI) {
@@ -29,7 +29,7 @@ angular.module('app.controllers', ['ionic','ngCordova'])
                 window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
             }
 
-            // 5
+            // 5 Cette fonction copie le fichier d'origine à notre répertoire app . Comme nous pourrions avoir à faire face à des images en double, nous donne un nouveau nom au fichier constitué d' une chaîne aléatoire et le nom original de l'image.
             function copyFile(fileEntry) {
                 var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
                 var newName = makeid() + name;
@@ -45,7 +45,7 @@ angular.module('app.controllers', ['ionic','ngCordova'])
                 fail);
             }
 
-            // 6
+            // 6 quand la copie est terminée avec succès on envoie l'url de l'image à notre tableau d'images. Ne pas oublier "appyl()" pour mettre à jour le tableau et la vue
             function onCopySuccess(entry) {
                 $scope.$apply(function () {
                     $scope.images.push(entry.nativeURL);
